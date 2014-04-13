2fcc0d7cd1e4b1cae4235691ce2411de4e3bf472
Abhi <abhi@abhi.gd>
2014-04-13 14:22:52 +0530

    Load config based on environment

--- run.py
+++ run.py
@@ -1,5 +1,30 @@
 #/usr/bin/python
+from optparse import OptionParser
+import os
 
-from app import app
+from app import app, startup
 
-app.run('0.0.0.0', 5000, debug=True)
+def main(environment):
+    port = 5000
+    if options.environment == "heroku":
+        port = int(os.getenv("PORT"))
+
+    class Config(object):
+        def __init__(self, environemnt, port):
+            self.ENVIRONMENT = environment
+            self.LOGGER_NAME = "bigboy"
+            self.PORT = port
+
+    config = Config(environment, port)
+    app.config.from_object(config)
+    startup()
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-e", "--env", dest="environment",
+                      help="ENV in which bigboy will run",
+                      default="app", metavar="ENV")
+    (options, args) = parser.parse_args()
+
+    main(options.environment)
