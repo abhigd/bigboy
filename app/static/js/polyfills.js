94c242708a76e1087c6005b3e0ec3a63d4e41378
Abhi <abhi@abhi.gd>
2014-11-09 00:07:40 +0530

    polypill for ends with

--- app/static/js/polyfills.js
+++ app/static/js/polyfills.js
@@ -8,6 +8,20 @@ if ( !Array.prototype.forEach ) {
 
 if ( !File.prototype.slice ) {
     if ( File.prototype.webkitSlice ) {
-        File.prototype.slice = File.prototype.webkitSlice
+        File.prototype.slice = File.prototype.webkitSlice;
     }
+}
+
+if (!String.prototype.endsWith) {
+  Object.defineProperty(String.prototype, 'endsWith', {
+    value: function (searchString, position) {
+      var subjectString = this.toString();
+      if (position === undefined || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+    }
+  });
 }
