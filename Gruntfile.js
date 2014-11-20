ad48ee90e31fc13b3d58e6ed741af92fc0cb8f32
Abhi <abhi@abhi.gd>
2014-11-20 21:23:31 +0530

    copy bootstrap and el-font to css folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,11 @@ module.exports = function(grunt) {
           {src: "vendor/crypto-js-evanvosberg/build/rollups/md5.js", dest: "app/static/js/md5.js"},
           {src: "vendor/crypto-js-evanvosberg/build/components/enc-base64-min.js", dest: "app/static/js/enc-base64-min.js"},
           {src: "vendor/crypto-js-evanvosberg/src/lib-typedarrays.js", dest: "app/static/js/lib-typedarrays.js"},
-          {src: "vendor/aws-sdk/aws-sdk.js", dest: "app/static/js/aws-sdk.js"}
+          {src: "vendor/aws-sdk/aws-sdk.js", dest: "app/static/js/aws-sdk.js"},
+          {src: "less/*", dest: "app/static/css/bootstrap", expand: true,
+            cwd: "bower_components/bootstrap"},
+          {src: "less/*", dest: "app/static/css/elusive-iconfont", expand: true,
+            cwd: "bower_components/elusive-iconfont"}
         ]
       }
     }
